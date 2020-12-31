1952416be2ab9c5fb16ec93d6da359511bd564f3
Delehach <y.delehach@gmail.com>
2020-12-31 17:17:42 +0200

    fix code formatting

--- secret/src/Dropdown/Dropdown.tsx
+++ secret/src/Dropdown/Dropdown.tsx
@@ -2,41 +2,41 @@ import React, {ReactNode, useLayoutEffect, useRef, useState} from 'react';
 import './Dropdown.css';
 
 interface DropdownProps {
-	target: ReactNode;
-	children: ReactNode;
+  target: ReactNode;
+  children: ReactNode;
 }
 
 export function Dropdown({target, children}: DropdownProps) {
-	const [dropdownDisplay, setDropdownDisplay] = useState<boolean>(false);
-	const [dropdownPosition, setDropDownPosition] = useState<string>('auto');
-	const buttonRef = useRef<HTMLButtonElement>(null);
-	const dropdownRef = useRef<HTMLDivElement>(null);
+  const [dropdownDisplay, setDropdownDisplay] = useState<boolean>(false);
+  const [dropdownPosition, setDropDownPosition] = useState<string>('auto');
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-	const dropdownStyles = {
-		display: dropdownDisplay ? `block` : `none`,
-		bottom: dropdownPosition,
-	}
+  const dropdownStyles = {
+    display: dropdownDisplay ? `block` : `none`,
+    bottom: dropdownPosition,
+  }
 
-	useLayoutEffect(() => {
-		if(buttonRef && buttonRef.current && dropdownRef && dropdownRef.current) {
-			const btnData = buttonRef.current.getBoundingClientRect();
-			const dropdownData = dropdownRef.current.getBoundingClientRect();
-			const totalDropdownHeight = btnData.top + btnData.height + dropdownData.height;
-			if (totalDropdownHeight > window.innerHeight) {
-				setDropDownPosition(`${btnData.height}px`);
-			}
-		}
-	}, [dropdownDisplay])
+  useLayoutEffect(() => {
+    if (buttonRef && buttonRef.current && dropdownRef && dropdownRef.current) {
+      const btnData = buttonRef.current.getBoundingClientRect();
+      const dropdownData = dropdownRef.current.getBoundingClientRect();
+      const totalDropdownHeight = btnData.top + btnData.height + dropdownData.height;
+      if (totalDropdownHeight > window.innerHeight) {
+        setDropDownPosition(`${btnData.height}px`);
+      }
+    }
+  }, [dropdownDisplay]);
 
-	return (
-		<div className='dropdown'>
-			<button
-				onClick={() => setDropdownDisplay(!dropdownDisplay)}
-				ref={buttonRef}
-			>
-				{target}
-			</button>
-			<div className='dropdown__content' ref={dropdownRef} style={dropdownStyles}>{children}</div>
-		</div>
-	);
+  return (
+    <div className='dropdown'>
+      <button
+        onClick={() => setDropdownDisplay(!dropdownDisplay)}
+        ref={buttonRef}
+      >
+        {target}
+      </button>
+      <div className='dropdown__content' ref={dropdownRef} style={dropdownStyles}>{children}</div>
+    </div>
+  );
 }
