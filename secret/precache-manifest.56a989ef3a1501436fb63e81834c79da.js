c4d39c5ac018f84011768c1a148de3e2e7110d18
Delehach <y.delehach@gmail.com>
2020-12-31 16:48:06 +0200

    added new demo

---
+++ secret/precache-manifest.56a989ef3a1501436fb63e81834c79da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af846af6ab8716967c326b9e69b7af8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29c1456659c22b631ddd",
+    "url": "/static/css/main.385a97e6.chunk.css"
+  },
+  {
+    "revision": "a8530641287d20723428",
+    "url": "/static/js/2.a13e68d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a13e68d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c1456659c22b631ddd",
+    "url": "/static/js/main.6d188d72.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
