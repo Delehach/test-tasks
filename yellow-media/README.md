cf525ccc3e13844a1e69f9032d7113fce1955453
Delehach <y.delehach@gmail.com>
2021-01-12 11:46:15 +0200

    added Yellow Media test task

---
+++ yellow-media/README.md
@@ -0,0 +1,19 @@
+# Yellow Media test task
+
+## Project setup
+```
+npm i
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Demo
+[Yellow Media test task demo](https://delehach.github.io/test-tasks/yellow-media-demo)
---
+++ yellow-media/src/components/Task_3.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="task">
+    <h2>Task 3</h2>
+    <pre>{{ urlParams }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task_3',
+  data: () => ({
+    urlParams: '',
+  }),
+  mounted() {
+    const searchParams = new URLSearchParams(window.location.search);
+    const obj = {};
+    for (const pair of searchParams.entries()) {
+      obj[pair[0]] = pair[1]
+    }
+    this.urlParams = JSON.stringify(obj, null, 2);
+  }
+}
+</script>
